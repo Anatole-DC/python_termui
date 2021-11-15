7beac2e9d4c36869a2ff48fd212734e78ee12b1f
Anatole-DC <adechauveron@gmail.com>
2021-11-15 18:48:57 +0100

    :art: Improvement of the code

--- src/animations/loaders.py
+++ src/animations/loaders.py
@@ -2,14 +2,12 @@ from src.animations.loading_tools import generate_grid, update_line
 from time import sleep
 
 def test():
-    generate_grid()
-    sleep(1)
+    generate_grid("[---------]")
+    sleep(0.5)
 
     bar = "█"
     for i in range(1, 10):
-        update_line(1, f"{bar * i}")
-        update_line(4, f"{bar * i}")
-        update_line(5, f"{bar * i}")
-        sleep(0.5)
-
-    sleep(1)
+        update_line(1, f"[{bar * i}")
+        update_line(4, f"[{bar * i}")
+        update_line(5, f"[{bar * i}")
+        sleep(0.1)
--- src/animations/loading_tools.py
+++ src/animations/loading_tools.py
@@ -2,6 +2,7 @@
 @TODO
 The goal is to create a y axis, on which we will know the coordinates of each line
 
+
 #1    #####################################################
 #2      Some text
 #2      ```                 ____  +++
@@ -14,7 +15,10 @@ The goal is to create a y axis, on which we will know the coordinates of each li
 #10   #####################################################
 
 
-Then, we will have to create a function update line, that will update(str) the line with the proper modifications.
+(done) Then, we will have to create a function update line, that will update(str) the line with the proper modifications.
+(    ) We must add a feature that will only update some part of the line, and keep the rest of the line.
+(    ) Create a progress bar with the percentages indicated and the progression is controlled with a parameter.
+(    ) Create a grid with a character whose position is updated with the arrow
 
 """
 
@@ -22,12 +26,12 @@ from time import sleep
 
 UP = "\033[F"
 
-def generate_grid(number_of_lines: int=10) ->  None:
-    for i in range(0, number_of_lines):
-        print(f"")
+def generate_grid(grid_content: str="", height: int=10) ->  None:
+    for i in range(0, height):
+        print(f"{grid_content}")
 
 def update_line(line_number: int, update: str) -> None:
-    print(f"{line_to_go_up(line_number)}#{line_number}{update}{go_back_to_end(line_number)}")
+    print(f"{line_to_go_up(line_number)}{update}{go_back_to_end(line_number)}")
 
 def line_to_go_up(line_number: int, total_number_of_lines: int=10) -> str:
     return UP * (total_number_of_lines - line_number + 1)
